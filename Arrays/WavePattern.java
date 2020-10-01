18ab904c044f0e56d74671d2cedf4da3084934ec
Manan Modi <mananmodi.0108@gmail.com>
2020-10-01 20:08:28 +0530

    Wave print in a Matrix

--- Arrays/WavePattern.java
+++ Arrays/WavePattern.java
@@ -1,5 +1,5 @@
 package Arrays;
-
+#Create a Package Array before Running
 public class WavePattern {
     public static void main(String[] args) {
         int[][] arr = {{11, 12, 13, 14}, {21, 22, 23, 24}, {31, 32, 33, 34}, {41, 42, 43, 44}};
